87e8707b5a98be82dc872669c8fcc28019195952
Mohammad <mohammad.rfn98@gmail.com>
2025-12-10 22:40:08 +0330

    save

--- resources/views/reports/index.blade.php
+++ resources/views/reports/index.blade.php
@@ -82,7 +82,7 @@
                             $statusColor = match($report->status){
                             'approved' => 'bg-green-100 text-green-700',
                             'rejected' => 'bg-red-100 text-red-700',
-                            'pending' => 'bg-yellow-100 text-yellow-700',
+                            'pending' => '÷bg-yellow-100 text-yellow-700',
                             default => 'bg-gray-100 text-gray-700'
                             };
                             @endphp
