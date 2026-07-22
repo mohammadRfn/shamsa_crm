90fbd530a90accd0d7097d039a0001077d8e777b
Mohammad <mohammad.rfn98@gmail.com>
2026-07-22 16:20:20 +0330

    list-change

--- resources/views/workrequests/index.blade.php
+++ resources/views/workrequests/index.blade.php
@@ -254,7 +254,7 @@
                                 <svg class="w-3.5 h-3.5" fill="none" stroke="currentColor" viewBox="0 0 24 24" stroke-width="2">
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M9.75 17L9 20l-1 1h8l-1-1-.75-3M3 13h18M5 17h14a2 2 0 002-2V5a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
                                 </svg>
-                                <span class="list-meta-val">{{ Str::limit($request->device_model, 12) }}</span>
+                                <span class="list-meta-val">{{ $request->device_model }}</span>
                             </div>
                             <div class="list-meta-item hidden xl:flex">
                                 <svg class="w-3.5 h-3.5" fill="none" stroke="currentColor" viewBox="0 0 24 24" stroke-width="2">
